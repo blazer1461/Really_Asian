de300bb9c2652441cab284135aa4f7029d69d120
Your Name <you@example.com>
2015-07-21 11:34:11 -0400

    sdf

--- API/weatherchinese.py
+++ API/weatherchinese.py
@@ -1,1 +1,11 @@
-import 
+import urllib2
+import json
+
+def get_weather(city):
+    url= "http://api.openweathermap.org/data/2.5/weather?q=" +city
+    u= urllib2.urlopen(url)
+    result= u.read
+    w= json.load(result)
+    return w
+
+print get_weather("nyc")
