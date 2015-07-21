e68a69b0c1227c09d726326bf7b9c7cd514d92d1
Your Name <you@example.com>
2015-07-21 14:06:45 -0400

    push

--- API/FlaskWeather.py
+++ API/FlaskWeather.py
@@ -1,13 +1,23 @@
 __author__ = 'blazer1461'
 import Weather
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 Asian_Weather= Flask(__name__)
 
-@Asian_Weather.route('/<city>')
-def city(city):
-    temp= Weather.get_weather(city)
-    return render_template("asian.html", temp_weather= temp)
+@Asian_Weather.route("/", methods=["POST", "GET"])
+def main_page():
+    if request.method == "GET":
+        return render_template("base.html", city= "Main Page")
+    elif request.method == "POST":
+        city= request.form["location"]
+        temp= Weather.get_weather(city)
+        if temp > 80:
+            location=  "kitchen"
+        elif temp<80 and temp > 60:
+            location= "dining room"
+        else:
+            location= "cellar"
+        return render_template("weatherincity.html", temp_weather=temp, city=city +" weather", house= location , location=city)
 
 if __name__ == "__main__":
 
