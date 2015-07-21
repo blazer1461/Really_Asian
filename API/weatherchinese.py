d8bf9469fd1b4f6c2a5af44a0133c5d7e0197228
Your Name <you@example.com>
2015-07-21 11:44:05 -0400

    Push

--- API/weatherchinese.py
+++ API/weatherchinese.py
@@ -4,8 +4,8 @@ import json
 def get_weather(city):
     url= "http://api.openweathermap.org/data/2.5/weather?q=" +city
     u= urllib2.urlopen(url)
-    result= u.read
-    w= json.load(result)
-    return w
+    result= u.read();
+    w= json.loads(result)
+    return w["main"]["temp"]
 
-print get_weather("nyc")
+
